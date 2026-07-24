f10049269e5d81fbde30311210d138ae23963cb6
JhonatanHe11 <jhonatan.he56@gmail.com>
2026-07-23 21:22:56 -0500

    Actualizacion sistema

--- API_Whatsapp/src/app.js
+++ API_Whatsapp/src/app.js
@@ -8,8 +8,10 @@ const {
 } = require('./whatsapp');
 
 const app = express();
+const VERIFY_TOKEN = "NexoraWebhook123";
 app.use(cors());
 app.use(express.json());
+
 app.post('/api/solicitud', async (req, res) => {
 
     try {
@@ -51,6 +53,26 @@ ${descripcion}
 
     }
 
+});
+
+app.get('/webhook', (req, res) => {
+
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        return res.status(200).send(challenge);
+    }
+
+    return res.sendStatus(403);
+
+});
+
+app.post('/webhook', (req, res) => {
+    console.log("Webhook recibido");
+    console.log(req.body);
+    res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
